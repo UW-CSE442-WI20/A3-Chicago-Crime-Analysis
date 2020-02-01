14b8cc6a4561d680ddd8c5066ec9eb58eca0756e
amepkx <IdicIlTErMIa>
2020-02-01 14:40:58 -0800

    l

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ var svg = d3.select("body").append("svg")
         "translate(" + margin.left + "," + margin.top + ")");
 
 // get the data
-d3.csv("crime_number.csv", function(error, data) {
+d3.csv("./crime_number.csv", function(error, data) {
     if (error) throw error;
 
     // format the data
